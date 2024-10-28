2f02b112f32b060826051a35397c191b7251b5c0
rosuH <hi@rosuh.me>
2024-10-28 22:44:11 +0800

    refactor(build): update versioning approach and CI configurations
    - Updated build.gradle.kts to derive versionCode and versionName from environment properties instead of hardcoded major, minor, and patch values.
    - Moved version-related properties from gradle.properties and adjusted CI configurations with new environment variables.
    - Added environmental configurations in pr_pre_check.yml and publish.yml for improved build performance.
    
    # GENERATE BY https://aicommit.app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,17 +11,8 @@ plugins {
 /**
  * read version from gradle.properties
  */
-val majorVersion by properties
-val minorVersion by properties
-val patchVersion by properties
-
-fun getVersionCode(): Int {
-    return (majorVersion as String).toInt() * 10000 + (minorVersion as String).toInt() * 100 + (patchVersion as String).toInt()
-}
-
-fun getVersionName(): String {
-    return "$majorVersion.$minorVersion.$patchVersion"
-}
+val versionCodeFromEnv by properties
+val versionNameFromEnv by properties
 
 android {
     namespace = "me.rosuh.sieve"
@@ -31,8 +22,8 @@ android {
         applicationId = "me.rosuh.sieve"
         minSdk = 24
         targetSdk = 34
-        versionCode = getVersionCode()
-        versionName = getVersionName()
+        versionCode = versionCodeFromEnv?.toString()?.toInt() ?: 1
+        versionName = versionNameFromEnv?.toString() ?: "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
